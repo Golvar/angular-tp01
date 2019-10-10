90a6153f336dbbc951848a4c30752c33ea6534d7
Thomas Galocha <thomas.galocha@hotmail.fr>
2019-10-11 00:15:30 +0200

    Fin du TP01-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { FooterComponent } from './footer/footer.component';
+import { CompteComponent } from './compte/compte.component';
+import { TelPipe } from "./models/telpipe";
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { RecapComponent } from './recap/recap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TetiereComponent,
+    FooterComponent,
+    CompteComponent,
+    RecapComponent,
+    TelPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule, 
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export class AppBootstrapModule {}
